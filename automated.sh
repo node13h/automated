7f35dbc11ddf689a6f8679356b59a30d278bdbfb
Sergej Alikov <sergej.alikov@gmail.com>
2017-07-31 14:42:30 +0100

    Update license header

--- automated.sh
+++ automated.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# An automation tool
 # Copyright (C) 2016-2017 Sergej Alikov <sergej.alikov@gmail.com>
 
-# This program is free software: you can redistribute it and/or modify
+# This file is part of Automated.
+
+# Automated is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
--- facts/10-OS.sh
+++ facts/10-OS.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091,SC2034
 
+# Copyright (C) 2016-2017 Sergej Alikov <sergej.alikov@gmail.com>
+
+# This file is part of Automated.
+
+# Automated is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 declare pkg ver
 
 FACT_SYSTEMD=FALSE
+
 
 if cmd_is_available systemctl; then
     if systemctl --quiet is-active -- '-.mount'; then
